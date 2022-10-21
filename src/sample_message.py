f94218f9a47dfa57ad5f626dc2ffebaa581c6f08
adrianptho <59070225+adrianptho@users.noreply.github.com>
2022-10-21 11:36:59 +0100

    committed

--- src/sample_message.py
+++ src/sample_message.py
@@ -7,3 +7,13 @@ my_message = {"name": name, "message": message}
 
 if __name__ == "__main__":
     save_message(my_message)
+
+from save import save_message
+
+name = "Adrian"
+message = "I'm so confused"
+
+my_message = {"name": name, "message": message}
+
+if __name__ == "__main__":
+    save_message(my_message)
